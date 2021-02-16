c30235372f5319ed36447b9b79e2921d227cd5d8
Shad <65975514+FMaxime21@users.noreply.github.com>
2021-02-16 18:52:24 +0100

    Ajout d'une page de sélection de jeux et de la partie interface du morpion

--- app/src/main/java/com/example/dreamgames/Morpion.java
+++ app/src/main/java/com/example/dreamgames/Morpion.java
@@ -6,18 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Morpion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_morpion);
     }
 
     public void onClick(View v)
     {
-        Intent gameActivity = new Intent(MainActivity.this, Morpion.class);
+        Intent gameActivity = new Intent(Morpion.this, MainActivity.class);
         startActivity(gameActivity);
     }
 }
